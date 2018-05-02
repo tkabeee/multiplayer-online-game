d4800df3091e63f1938ea4355bbd345edebb04c0
tkabeee <tkabee@gmail.com>
2018-05-03 00:21:33 +0900

    use static dir

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express')
-const http = require('http')
-const io = require('socket.io')
-
 const app = express()
-const server = http.Server(app)
+const server = require('http').Server(app)
+const io = require('socket.io').listen(server)
 
-io.listen(server)
+app.use('/css', express.static(__dirname + '/css'))
+app.use('/js', express.static(__dirname + '/js'))
+app.use('/assets', express.static(__dirname + '/assets'))
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html')
 })
 
-server.listen(8081, function(){
+server.listen(process.env.PORT || 8081, function(){
   console.log('Listening on ' + server.address().port)
 })
