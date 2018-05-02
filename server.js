13b30bad1df23eec17a50723a11e27fce4725634
tkabeee <tkabee@gmail.com>
2018-05-02 23:48:35 +0900

    express + socket.io server

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-import express from 'express'
-import http from 'http'
-import io from 'socket.io'
+const express = require('express')
+const http = require('http')
+const io = require('socket.io')
 
 const app = express()
 const server = http.Server(app)
 
 io.listen(server)
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html')
+})
 
 server.listen(8081, function(){
   console.log('Listening on ' + server.address().port)
